713ee72bc8cb5bc6218f49c3b46c7d0dd85bf3a3
Carmen Macleroy Obied, PhD <carmen.obied@gmail.com>
2020-08-03 00:08:52 -0700

    refactored code for routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,14 +3,14 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const express = require("express");
 
-const Workout = require("../models/Workout");
+const Workout = require("../models/workout");
 
 // workouts page
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .sort({ day: 1 })
-    .then(workoutdb => {
-      res.json(workoutdb);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -19,8 +19,8 @@ router.get("/api/workouts", (req, res) => {
 
 router.post("/api/workouts", ({ body }, res) => {
   Workout.create(body)
-    .then(workoutdb => {
-      res.json(workoutdb);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -30,8 +30,8 @@ router.post("/api/workouts", ({ body }, res) => {
 // completed workouts
 router.post("/api/workouts", (req, res) => {
   Workout.insertMany(body)
-    .then(workoutdb => {
-      res.json(workoutdb);
+    .then(dbWorkout => {
+      res.json(dbWorkout);
     })
     .catch(err => {
       res.status(400).json(err);
@@ -43,16 +43,16 @@ router.put("/api/workouts/:id", (req, res) => {
   Workout.update(
     { _id: req.params.id },
     { $set: { exercises: [req.body] } }
-  ).then((workoutdb) => {
-    res.json(workoutdb);
+  ).then((dbWorkout) => {
+    res.json(dbWorkout);
   });
 });
 
 // range page
 router.get("/api/workouts/range", (req, res) => {
   Workout.find({})
-  .then(workoutdb => {
-      res.json(workoutdb);
+  .then(dbWorkout => {
+      res.json(dbWorkout);
   })
     .catch(err => {
       res.status(400).json(err);
